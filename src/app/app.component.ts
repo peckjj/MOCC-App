c69e63367fce9178ff1fd1872511e5d470f81e59
bddagoat <davisb21@hsc.edu>
2021-11-13 10:43:32 -0500

    css update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,10 @@
 import { Component } from '@angular/core';
 
-// Hello
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'MOCC-client-app';
+  title = 'MOCC-App';
 }
