846083fa477bb6f7ee1caeccda8f2a281140e892
bddagoat <davisb21@hsc.edu>
2021-12-06 23:04:53 -0500

    touch up

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -7,9 +7,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  // guser;
+  // constructor(
+  //   ngZone: NgZone
+  // ) {
+  //   window: ['onSignIn'] = user => ngZone.run(
+  //     () => {
+  //       this.afterSignUp(user);
+  //     }
+  //   )
+  // }
 
   ngOnInit(): void {
   }
+
+  // afterSignUp(googleUser) {
+  //   this.guser = googleUser;
+  // }
 
 }
