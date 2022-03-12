07075a54ca603b7c77bf9fcffcc8195e1c8fb2c8
bddagoat <davisb21@hsc.edu>
2022-03-11 22:47:25 -0500

    prospect entry

--- src/app/prospect/prospect.component.ts
+++ src/app/prospect/prospect.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { IProspect } from 'src/models/IProspect';
+import { HttpService } from '../http.service';
 
 @Component({
   selector: 'app-prospect',
@@ -7,9 +9,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProspectComponent implements OnInit {
 
+  searchResults: IProspect[];
+
+  notIncluded: string[] = [
+
+    "id",
+    "birthday",
+    "bio",
+    "status",
+    "password",
+
+  ]
+
+  prospect: string = "";
+
   constructor() { }
 
   ngOnInit(): void {
   }
+
+  search() {
+    this.http.searchByName(this.prospect).then(data => this.searchResults= data);
+  }
+
+  getIncluded(title: IProspect): string[] {
+    return Object.keys(title).filter( (data)=> !this.notIncluded.includes(data) );
 
 }
