9763e2989485b37f13942876fbfbeb9afbb4ab21
bddagoat <davisb21@hsc.edu>
2022-04-06 18:05:32 -0400

    form spacing

--- src/app/create-profile/create-profile.component.ts
+++ src/app/create-profile/create-profile.component.ts
@@ -7,6 +7,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CreateProfileComponent implements OnInit {
 
+  UserInfo: { }
+
+  // input = document.getElementById("password").innerHTML;
+  // value: string = "Retype Password:";
+
+  // passwordLabel = document.createElement("label");
+  
+
+  // password = document.createElement("input");
+
+  // retypePassword(input) {
+  //   if (input != 0) {
+  //     return console.log(this.passwordLabel);
+  //   }
+  // }
+
   constructor() { }
 
   ngOnInit(): void {
